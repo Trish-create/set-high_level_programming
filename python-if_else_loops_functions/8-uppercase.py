aa2c5666c28423477c951a7469986ca0d5806638
root <root@DESKTOP-7489QJ0.localdomain>
2026-07-14 14:20:11 +0200

    new tasks of loops

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for letter in str:
+        if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
+            print("{}".format(chr(ord(letter) - 32)), end="")
+        else:
+             print("{}".format(letter), end="")
+    print()
